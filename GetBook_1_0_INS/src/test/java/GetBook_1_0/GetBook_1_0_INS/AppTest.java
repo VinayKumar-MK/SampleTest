b633a6e824741eb91f1f0794f1303bbb4267cfdc
vinay_kumar2 <vinay_kumar2@TH1926.thbs.india.com>
2019-12-20 12:39:03 +0530

    Books project is on fly

---
+++ GetBook_1_0_INS/src/test/java/GetBook_1_0/GetBook_1_0_INS/AppTest.java
@@ -0,0 +1,20 @@
+package GetBook_1_0.GetBook_1_0_INS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
